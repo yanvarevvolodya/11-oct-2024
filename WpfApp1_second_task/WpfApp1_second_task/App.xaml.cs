491321db75e632351e06b9f30daa5fe6c868da75
yanvarevvolodya <yanvarev2018@mail.ru>
2024-10-18 09:50:02 +0500

    выполнено 2 задание

---
+++ WpfApp1_second_task/WpfApp1_second_task/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp1_second_task
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ WpfApp1_second_task/WpfApp1_second_task/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1_second_task
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ColorComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (ColorComboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string colorName = selectedItem.Tag.ToString();
+                Color color = (Color)ColorConverter.ConvertFromString(colorName);
+                DrawingCanvas.DefaultDrawingAttributes.Color = color;
+            }
+        }
+
+        private void BrushSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double brushSize = BrushSizeSlider.Value;
+
+            DrawingCanvas.DefaultDrawingAttributes.Width = brushSize;
+            DrawingCanvas.DefaultDrawingAttributes.Height = brushSize;
+        }
+    }
+}
